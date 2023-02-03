859b5c6e461f3f098c0a9e2a043a3da495a93788
maiSamirMohammad <88589466+maiSamirMohammad@users.noreply.github.com>
2023-02-03 21:59:02 +0200

    ui favorits screen and guest popup

--- app/src/main/java/com/example/foodplanner/LoginActivity.java
+++ app/src/main/java/com/example/foodplanner/LoginActivity.java
@@ -3,12 +3,19 @@ package com.example.foodplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
     }
 }
