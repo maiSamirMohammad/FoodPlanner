d4b6215dc772258e55ddbcce2f4888c790841c61
AHMHAM <ahmedhambouta@gmail.com>
2023-02-16 18:08:53 +0200

    Add Navigation Component

--- app/src/main/java/com/example/foodplanner/network/RetrofitClient.java
+++ app/src/main/java/com/example/foodplanner/network/RetrofitClient.java
@@ -1,7 +1,5 @@
 package com.example.foodplanner.network;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
